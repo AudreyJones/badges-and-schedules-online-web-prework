bce202de725b4f703a1a2c9060d39dde10b448f5
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-01 17:38:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def batch_badge_creator(attendees)
+<<<<<<< HEAD
   attendees.map {|i| badge_maker(i)}
 end
 
@@ -16,4 +17,7 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).map {|i| puts i}
   assign_rooms(attendees).map {|i| puts i}
+=======
+  return badge_maker(attendees)
+>>>>>>> 7a30e74cd7cb77bd305e5975ed8dde4a46df8132
 end
